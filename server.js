00132ec719b3738b90f1d881739ffdf352f57359
Roan OSullivan <roanosullivan@sosuke.729x.com>
2015-01-12 11:15:51 -0500

    Forgot to include server.js in prev commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000);
+console.log('Server running at http://localhost:3000/');
